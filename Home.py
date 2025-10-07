6f9be85cd4b0fbd8d95b2e378352999477591210
Victor Druyts <82957329+Victor-Druyts@users.noreply.github.com>
2025-10-07 19:33:59 +0200

    float input voor verbruik

--- Home.py
+++ Home.py
@@ -10,14 +10,13 @@ connection = sqlite3.connect(db_path)
 c = connection.cursor()
 
 
-
 @st.cache_data
 def qeury_db():
     c.execute("SELECT DISTINCT Company_Names FROM cars ORDER BY Company_Names")
     merken = [row[0] for row in c.fetchall()]
     return merken
 
-geselecteerdMerk, geselecteerdModel = st.columns(2, vertical_alignment="bottom")
+geselecteerdMerk, geselecteerdModel, verbruik = st.columns(3, vertical_alignment="bottom")
 
 merken = qeury_db()
 geselecteerdMerk = geselecteerdMerk.selectbox("Kies een merk", merken, index=None, placeholder="Kies een merk")
@@ -30,7 +29,4 @@ else:
 
 geselecteerdModel = geselecteerdModel.selectbox("Kies een model", modellen, index=None, placeholder="Kies een model")
 
-#merken, modelen = qeury_db()
-#merk, model = st.columns(2, vertical_alignment="bottom")
-#merk.selectbox("Selecteer een merk", options=merken, index=None, placeholder="Kies een merk")
-#model.selectbox("Selecteer een model", options=modelen, index=None, placeholder="Kies een model")
+verbruik = verbruik.number_input("Kies een verbruik (l/100km) of (kwh/100km)", min_value=0.0,step=1.0)
