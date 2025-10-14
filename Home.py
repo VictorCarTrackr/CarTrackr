77d9963758cc8e5798c545a5d21364447a049b9a
Victor Druyts <82957329+Victor-Druyts@users.noreply.github.com>
2025-10-14 12:47:16 +0200

    tabel en grafieken

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import streamlit as st
 import sqlite3
+import pandas as pd
 
 
 
@@ -30,13 +31,13 @@ electriciteitPrijs = 0.40  #Prijs per kWh elektriciteit
 
 st.set_page_config(page_title=paginaTitel, page_icon=favicon, layout="wide")
 
-
-#Login met microsoft
-if not st.user.is_logged_in:
-    st.button("Login met microsoft", on_click=lambda:st.login("microsoft"))
-else:
-    st.write(f"Welkom {st.user.name}!")
-    st.button("Log uit", on_click=lambda:st.logout())
+with st.sidebar:
+    #Login met microsoft
+    if not st.user.is_logged_in:
+        st.button("Login met microsoft", on_click=lambda:st.login("microsoft"))
+    else:
+        st.write(f"Welkom {st.user.name}!")
+        st.button("Log uit", on_click=lambda:st.logout())
 
 
 st.title("Voeg je auto toe!")
@@ -83,29 +84,49 @@ def totaalKostenBerekenen(totaalVerbruik, brandstofType):
     if brandstofType == "Petrol" or brandstofType == "Plug-in Hybrid":
         totaalKosten = totaalVerbruik * benzinePrijs
         st.markdown(f"Je benzine kosten zijn ongeveer €{totaalKosten:,.2f}")
-        st.markdown("Je hebt een benzine auto.")
+        brandstofType = "Benzine"
+        st.markdown(f"Je hebt een {brandstofType} auto.")
     elif brandstofType == "Diesel":
         totaalKosten = totaalVerbruik * dieselPrijs
         st.markdown(f"Je diesel kosten zijn ongeveer €{totaalKosten:,.2f}")
         st.markdown("Je hebt een diesel auto.")
+        brandstofType = "Diesel"
+        st.markdown(f"Je hebt een {brandstofType} auto.")
     else:
         totaalKosten = totaalVerbruik * electriciteitPrijs
         st.markdown(f"Je elektriciteits kosten zijn ongeveer €{totaalKosten:,.2f}")
         st.markdown("Je hebt een elektrische of semi elektrische auto.")
-    return totaalKosten
+        brandstofType = "Elektriciteit"
+        st.markdown(f"Je hebt een {brandstofType} auto.")
+    return totaalKosten, brandstofType
 
-def submitButton(geselecteerdModel, verbruik, aantalKm):
+def submitButton(geselecteerdModel, verbruik, aantalKm, brandstofType):
     if geselecteerdMerk and geselecteerdModel and verbruik is not None and aantalKm is not None:
         #Statistieken berekenen
         totaalVerbruik = (verbruik / 100) * aantalKm
         if verbruik <= 0:
             st.error("Het verbruik moet groter zijn dan 0.")
         else:
-            totaalKostenBerekenen(totaalVerbruik, brandstofType)
+            totaalKosten, brandstofType = totaalKostenBerekenen(totaalVerbruik, brandstofType)
             st.markdown(f"Je hebt al ongeveer {totaalVerbruik:,.2f} liter verbruikt voor {aantalKm:,.2f} kilometer.")
+            tabel = {
+                "Merk": geselecteerdMerk,
+                "Model": geselecteerdModel,
+                "Brandstof type": brandstofType,
+                "Verbruik (l/100km) of (kwh/100km)": float(verbruik),
+                "Aantal kilometers": int(aantalKm),
+                "Totaal verbruik (l) of (kwh)": float(totaalVerbruik),
+                "Totaal kosten (€)": float(totaalKosten)
+            }
+            df = pd.DataFrame([tabel], index=None)
+            st.write(df)
+            kostenGrafiek, kilometerGrafiek = st.columns(2, vertical_alignment="bottom")
+            kilometerGrafiek = st.bar_chart(df.loc[:, ["Aantal kilometers", "Model"]].set_index("Model"), height=400)
+            kostenGrafiek = st.bar_chart(df.loc[:, ["Totaal kosten (€)", "Model"]].set_index("Model"),  height=400)
     else:
         st.error("Vul alle velden in om de statistieken te berekenen.")
 
+
 #Statistieken knop
 if st.button("Bereken statistieken van je auto!"):
-    submitButton(geselecteerdModel, verbruik, aantalKm)
+    submitButton(geselecteerdModel, verbruik, aantalKm, brandstofType)
